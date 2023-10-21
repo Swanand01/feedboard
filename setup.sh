a0a6d6bb86bc1d5422c60da359aa81457218fea4
Swanand Mathekar <swanandmathekar@gmail.com>
2023-10-21 15:33:22 +0530

    Add entrypoint script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,23 @@
-docker-compose exec web python manage.py migrate --no-input
-docker-compose exec web python manage.py createsuperuser
+#!/bin/bash
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# Run migrations in the background
+python manage.py migrate --no-input &
+
+# Wait for the migrate process to finish
+while ! python manage.py migrate | grep "No migrations to apply"; do
+    sleep 1
+done
+
+python manage.py createsuperuser --no-input
+gunicorn feedboard.wsgi:application --bind 0.0.0.0:8000
