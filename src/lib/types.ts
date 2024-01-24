36de539dfaffde492aa66301ed7e3fdbb71bb4ce
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-24 21:27:55 +0530

    Add types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,4 @@
+import { Prisma, Category as PrismaCategory, Post as PrismaPost } from "@prisma/client";
+
+export type Category = PrismaCategory & Partial<Prisma.CategoryGetPayload<{ include: { statuses: true } }>>
+export type Post = PrismaPost & Partial<Prisma.PostGetPayload<{ include: { _count: { select: { upvotes: true } } } }>>
