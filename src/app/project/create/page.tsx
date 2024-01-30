fb09845119ff6d9e2b6aa702964a064de1d08061
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-30 16:40:26 +0530

    Update styling and add className prop

--- src/app/project/create/page.tsx
+++ src/app/project/create/page.tsx
@@ -2,5 +2,9 @@ import CreateProjectForm from "@/components/project/form";
 
 export default function Page() {
     // Protect this route
-    return <CreateProjectForm />;
+    return (
+        <div className="w-full md:w-96 mx-auto">
+            <CreateProjectForm />
+        </div>
+    );
 }
