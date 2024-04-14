676e95daa7d567af0d09130f38ddfe5f0b6e47f6
Swanand01 <swanandmathekar@gmail.com>
2024-04-14 23:41:51 +0530

    Add Google Provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,98 +1,89 @@
-import bcrypt from 'bcrypt';
-import prisma from '@/lib/prisma';
-import { User } from '@prisma/client'
+import bcrypt from "bcrypt";
+import prisma from "@/lib/prisma";
+import { User } from "@prisma/client";
 import { NextAuthOptions } from "next-auth";
-import { getServerSession } from "next-auth"
-import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next"
-import CredentialsProvider from "next-auth/providers/credentials";
+import { getServerSession } from "next-auth";
+import type {
+    GetServerSidePropsContext,
+    NextApiRequest,
+    NextApiResponse,
+} from "next";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
     session: {
-        strategy: 'jwt'
+        strategy: "jwt",
     },
     providers: [
-        CredentialsProvider({
-            name: 'Sign In',
-            credentials: {
-                email: {
-                    label: 'Email',
-                    type: 'email',
-                    placeholder: 'hello@example.com'
-                },
-                password: {
-                    label: 'Password',
-                    type: 'password'
-                }
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials.password) {
-                    return null;
-                }
-
-                const user = await prisma.user.findUnique({
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            async profile(profile) {
+                const user: User = await prisma.user.findUnique({
                     where: {
-                        email: credentials.email
-                    }
-                })
-
-                if (!user) {
-                    return null;
-                }
-
-                const passwordValid = await bcrypt.compare(
-                    credentials.password,
-                    user.password
-                )
-
-                if (!passwordValid) {
-                    return null;
-                }
-
+                        email: profile.email,
+                    },
+                });
                 return {
                     id: user.id,
                     username: user.username,
                     email: user.email,
                     dateJoined: user.dateJoined,
-                    isSuperuser: user.isSuperuser
+                    isSuperuser: user.isSuperuser,
                 };
-            }
-        })
+            },
+        }),
     ],
     callbacks: {
+        async signIn({ account, profile }) {
+            if (account?.provider === "google") {
+                if (!profile?.name || !profile.email) return false;
+
+                const existingUserByEmail = await prisma.user.findUnique({
+                    where: {
+                        email: profile?.email,
+                    },
+                });
+                if (!existingUserByEmail) {
+                    await prisma.user.create({
+                        data: {
+                            username: profile.name,
+                            email: profile.email,
+                        },
+                    });
+                }
+            }
+            return true;
+        },
         jwt: ({ token, user }) => {
             if (user) {
-                const u = user as unknown as User;
-                return {
-                    ...token,
-                    id: u.id,
-                    username: u.username,
-                    email: u.email,
-                    dateJoined: u.dateJoined,
-                    isSuperuser: u.isSuperuser
-                }
+                return { ...token, ...user };
             }
             return token;
         },
-
         session: ({ session, token }) => {
             return {
                 ...session,
                 user: {
-                    ...session.user,
                     id: token.id,
                     username: token.username,
                     email: token.email,
                     dateJoined: token.dateJoined,
-                    isSuperuser: token.isSuperuser
-                }
-            }
-        }
+                    isSuperuser: token.isSuperuser,
+                },
+            };
+        },
     },
     pages: {
-        signIn: "/users/login"
-    }
-}
+        signIn: "/users/login",
+    },
+};
 
-export function getUserSession(...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []) {
-    return getServerSession(...args, authOptions)
+export function getUserSession(
+    ...args:
+        | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+        | [NextApiRequest, NextApiResponse]
+        | []
+) {
+    return getServerSession(...args, authOptions);
 }
