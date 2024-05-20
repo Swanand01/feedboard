ff24947dd5bd18b937f437d721e8eefac3c095b2
Swanand01 <swanandmathekar@gmail.com>
2024-05-21 01:07:45 +0530

    Improve `createSuperuser.ts`

--- src/lib/createSuperuser.ts
+++ src/lib/createSuperuser.ts
@@ -1,6 +1,36 @@
 import prisma from "./prisma";
 
-function main() {
+async function getUser(email: string) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+    return user;
+}
+
+async function createSuperuser(email: string) {
+    await prisma.user.create({
+        data: {
+            email,
+            username: email,
+            isSuperuser: true,
+        },
+    });
+}
+
+async function updateSuperuser(email: string) {
+    await prisma.user.update({
+        where: {
+            email,
+        },
+        data: {
+            isSuperuser: true,
+        },
+    });
+}
+
+async function main() {
     if (!process.env.SUPERUSER_EMAIL) {
         console.log("No SUPERUSER_EMAIL found in .env.");
         return;
@@ -8,37 +38,25 @@ function main() {
 
     const email = process.env.SUPERUSER_EMAIL;
 
-    prisma.user
-        .findFirst({
-            where: {
-                isSuperuser: true,
-            },
-        })
-        .then((existingSuperuser) => {
-            if (existingSuperuser) {
-                console.log("A Superuser already exists.");
-                return;
-            } else {
-                return prisma.user.create({
-                    data: {
-                        email,
-                        username: email,
-                        isSuperuser: true,
-                    },
-                });
-            }
-        })
-        .then((createdSuperuser) => {
-            if (createdSuperuser) {
-                console.log("Superuser created with the given credentials");
-            }
-        })
-        .catch((error) => {
-            console.error("Error creating superuser:", error);
-        })
-        .finally(async () => {
-            await prisma.$disconnect();
-        });
+    try {
+        const existingUser = await getUser(email);
+
+        if (!existingUser) {
+            await createSuperuser(email);
+            console.log("Superuser created with the given credentials.");
+        } else if (!existingUser.isSuperuser) {
+            await updateSuperuser(email);
+            console.log(
+                "The user's superuser status has been updated to true.",
+            );
+        } else {
+            console.log("The user is already a Superuser.");
+        }
+    } catch (error) {
+        console.error("Error creating or updating superuser:", error);
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
 main();
