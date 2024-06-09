92ef51dc288d72e1e99e360bf4b2d5dc0fb44415
Swanand01 <swanandmathekar@gmail.com>
2024-06-09 15:17:43 +0530

    Add update and delete routes for post

--- app/routes/post.update.tsx
+++ app/routes/post.update.tsx
@@ -1,5 +1,13 @@
-import { ActionFunctionArgs } from "@remix-run/node";
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { updatePost } from "~/lib/post/actions";
 
-export function action({ request }: ActionFunctionArgs) {
-  return null;
+export async function action({ request }: ActionFunctionArgs) {
+  const { postId, values } = await request.json();
+  const { success, message, post } = await updatePost(request, postId, values);
+  if (success && post) {
+    return redirect(
+      `/project/${post.category.project.slug}/${post.category.slug}/${post.slug}`,
+    );
+  }
+  return { success, message };
 }
