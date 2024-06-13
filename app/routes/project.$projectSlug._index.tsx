d5bae1ad80452ee75d0aebc26833dae6a59c3ede
Swanand01 <swanandmathekar@gmail.com>
2024-06-13 16:58:19 +0530

    Fix prose and prose-invert classes

--- app/routes/project.$projectSlug._index.tsx
+++ app/routes/project.$projectSlug._index.tsx
@@ -6,10 +6,12 @@ import { projectLoader } from "./project.$projectSlug";
 export default function Page() {
   const { slug, description, categories } = useOutletContext<projectLoader>();
   return (
-    <div className="flex flex-col gap-8 mt-8 prose dark:prose-invert">
-      <p className="mb-0">{description}</p>
-      <div className="flex flex-col gap-4 prose dark:prose-invert">
-        <h3>Boards</h3>
+    <div className="flex flex-col gap-8 mt-8">
+      <p className="mb-0 prose dark:prose-invert">{description}</p>
+      <div className="flex flex-col gap-4">
+        <div className="prose dark:prose-invert">
+          <h3>Boards</h3>
+        </div>
         <Boards categories={categories} />
       </div>
       <div className="flex flex-col gap-4">
